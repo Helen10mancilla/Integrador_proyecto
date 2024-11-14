5f15e860126717aeee0eeb90caef15fa5a2dc45f
Helen orejuela mancilla <135240673+Helen10mancilla@users.noreply.github.com>
2024-11-14 15:38:11 -0500

    archivos php
    Se agregan archivos php para la conexion con la base de datos

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+// Configuración de la base de datos
+$host = 'localhost'; // Cambia esto si es necesario
+$dbname = 'proyecto';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar con la base de datos: " . $e->getMessage());
+}
+
+
+
+
